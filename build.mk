f582fad77b12369cd860bb51d68c9aca8f02de23
Roel Drost <roelpuntdrost@gmail.com>
2023-01-15 16:17:40 +0100

    Added support for non-flat project

--- build.mk
+++ build.mk
@@ -1,53 +1,71 @@
+PARTS                   ?=
+IMAGES                  ?=
+
 # Paths
 DEPDIR                  ?= ./.deps
-STLDIR                  ?= ./stl
+PARTSDIR                ?= ./parts
+IMAGESDIR               ?= ./images
+
+# Extensions
+PART_SOURCE_EXTENSION       ?= .scad
+PART_DESTINATION_EXTENSION  ?= .stl
+IMAGE_SOURCE_EXTENSION      ?= .scad
+IMAGE_DESTINATION_EXTENSION ?= .png
 
 # Applications
 OPENSCAD                ?= openscad
 GIT                     ?= git
+OPENSCAD_ARGS            =
 
 # Git
 git_revision_nr         :=$(shell $(GIT) rev-parse HEAD 2> /dev/null || echo "")
 git_uncommitted_changes :=$(if $(shell $(GIT) status --untracked-files=no --porcelain 2> /dev/null || echo ""),+,)
 git_revision            :=$(git_revision_nr)$(git_uncommitted_changes)
 
-# Derived paths
-stl_folder_rev :=$(STLDIR)/$(git_revision)
+# Derived
+ifeq ($(git_revision_nr),)
+OPENSCAD_ARGS           += -D "GIT_REVISION=\"$(git_revision)\""
+endif
 
 # Targets
 .PHONY: all
-all: $(PARTS:%=$(STLDIR)/%.stl)
+all:
 
-clean:
-	rm -rf $(DEPDIR)
-	rm -rf $(filter-out $(dir $(STLDIR)/.), $(sort $(dir $(wildcard $(STLDIR)/*/))))
+.PHONY: all_parts
+all_parts: $(PARTS:%=$(PARTSDIR)/%.stl)
+all: all_parts
 
-cleanall:
-	rm -rf $(DEPDIR)
-	rm -rf $(STLDIR)
+.PHONY: all_images
+all_images: $(IMAGES:%=$(IMAGESDIR)/%.png)
+all: all_images
 
-open: $(PARTS:%=%.scad)
-	$(OPENSCAD) -D "GIT_REVISION=\"$(git_revision)?\"" $(PARTS:%=%.scad)
+.PHONY: clean_deps
+clean_deps:
+	rm -r $(DEPDIR)
 
-openall:
-	$(OPENSCAD) -D "GIT_REVISION=\"$(git_revision)?\"" $(shell find . -type f -name '*.scad') $(shell find . -type f -name '*.inc')
+.PHONY: clean_parts
+clean_parts:
+	rm -r $(PARTSDIR)
 
-ifeq ($(git_revision_nr),)
-$(STLDIR)/%.stl: %.scad
-$(STLDIR)/%.stl: %.scad $(DEPDIR)/%.d | $(DEPDIR) $(STLDIR)
-	$(OPENSCAD) -d $(DEPDIR)/$*.d -o $(STLDIR)/$*.stl $*.scad
-else
-$(STLDIR)/%.stl $(stl_folder_rev)/%.stl: %.scad
-$(STLDIR)/%.stl $(stl_folder_rev)/%.stl: %.scad $(DEPDIR)/%.d | $(DEPDIR) $(STLDIR)
-	$(OPENSCAD) -d $(DEPDIR)/$*.d -D "GIT_REVISION=\"$(git_revision)\"" -o $(STLDIR)/$*.stl $*.scad
-	mkdir -p $(stl_folder_rev)
-	cp $(STLDIR)/$*.stl $(stl_folder_rev)/$*.stl
-endif
+.PHONY: clean_images
+clean_images:
+	rm -r $(IMAGESDIR)
 
-$(DEPDIR) $(STLDIR): ; @mkdir -p $@
+clean: clean_deps clean_parts clean_images
 
-DEPFILES := $(PARTS:%=$(DEPDIR)/%.d)
+$(PARTSDIR)/%.stl: %$(PART_SOURCE_EXTENSION)
+$(PARTSDIR)/%.stl: %$(PART_SOURCE_EXTENSION) | $(DEPDIR)/%.d
+	mkdir -p $(DEPDIR)/$(dir $*)
+	mkdir -p $(PARTSDIR)/$(dir $*)
+	$(OPENSCAD) -d $(DEPDIR)/$*.d $(OPENSCAD_ARGS) -D "IS_PART=1" -o $(PARTSDIR)/$*$(PART_DESTINATION_EXTENSION) $*$(PART_SOURCE_EXTENSION)
+
+$(IMAGESDIR)/%.png: %$(IMAGE_SOURCE_EXTENSION)
+$(IMAGESDIR)/%.png: %$(IMAGE_SOURCE_EXTENSION) | $(DEPDIR)/%.d
+	mkdir -p $(DEPDIR)/$(dir $*)
+	mkdir -p $(IMAGESDIR)/$(dir $*)
+	$(OPENSCAD) -d $(DEPDIR)/$*.d $(OPENSCAD_ARGS) -D "IS_IMAGE=1" -o $(IMAGESDIR)/$*$(IMAGE_DESTINATION_EXTENSION) $*$(IMAGE_SOURCE_EXTENSION)
+
+DEPFILES := $(PARTS:%=$(DEPDIR)/%.d) $(IMAGES:%=$(DEPDIR)/%.d)
 $(DEPFILES):
-
 include $(wildcard $(DEPFILES))
 
